30588efb3ce3532adc03aa326b99604a2c9926da
Lisa <beth200210@gmail.com>
2023-10-17 19:30:20 +0300

    Lisasfiles

---
+++ src/pages/ProgrammsPage/components/ProgrammTile/ProgrammTile.jsx
@@ -0,0 +1,10 @@
+import Logo from "./KubsauLogo.svg"
+import "./ProgrammTile.scss"
+
+
+const ProgrammTile = () => {
+    return <div className="TileWrapper">    
+        <img src={Logo} />
+    </div>
+}
+export default ProgrammTile
