4d3467e9a4a4b7d33fde4deb5dafa3e81e52a792
AbShamilDev <abkarov02@gmail.com>
2023-12-15 18:52:27 +0300

    Подготовка анимации удаления

--- src/pages/TeacherPages/TeacherProgrammEditPage/LabTableRow.jsx
+++ src/pages/TeacherPages/TeacherProgrammEditPage/LabTableRow.jsx
@@ -8,7 +8,13 @@ const LabTableRow = (props) => {
     const deleteLabApi = `/lab/${props.id}`;
 
     const deleteLab = async () => {
-        await axios.delete(deleteLabApi).then(() => { document.querySelector('.labsTable').classList.toggle('delete'); setTimeout(() => {props.deleteLab(props.id); document.querySelector('.labsTable').classList.toggle('delete')}, 2000)}).catch((err) => {console.log(err)})
+        await axios.delete(deleteLabApi).then(() => {
+            document.querySelector('.deleteAnimWrapper').classList.toggle('show');
+            setTimeout(() => {
+                props.deleteLab(props.id);
+                document.querySelector('.deleteAnimWrapper').classList.toggle('show')
+            }, 2000)
+        }).catch((err) => { console.log(err) })
     }
 
     return <tr>
@@ -17,6 +23,9 @@ const LabTableRow = (props) => {
         <td>{`${beginDate.getDate() < 10 ? `0${beginDate.getDate()}` : beginDate.getDate()}.${beginDate.getMonth() + 1 < 10 ? `0${beginDate.getMonth() + 1}` : beginDate.getMonth() + 1}.${beginDate.getFullYear()}`}</td>
         <td>{`${deadLine.getDate() < 10 ? `0${deadLine.getDate()}` : deadLine.getDate()}.${deadLine.getMonth() + 1 < 10 ? `0${deadLine.getMonth() + 1}` : deadLine.getMonth() + 1}.${deadLine.getFullYear()}`}</td>
         <td>
+            <div className='deleteAnimWrapper'>
+                <div className='deleteAnim'> </div>
+            </div>
             <div onClick={deleteLab} className='labActions'>
                 делит
             </div>
